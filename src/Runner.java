d018b4f0e1e9229de639a16e9a4f3a79a8f57e3c
Wei Xi Huang <weixih@nycstudents.net>
2022-10-12 18:01:03 -0400

    e

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,8 @@
 public class Runner {
     public static void main(String[]args){
-        String str = new String("Hi, my name is Jeff.");
-        String str1 = str.substring(0,2);
-        String str2 = str.substring(4);
-
-        System.out.println(str1);
-        System.out.println(str2);
+        String str1 = new String("hi");
+        String str2 = new String("Hi");
+        String str3 = new String("hi");
+        String str4 = new String("");
     }
 }
